c9e15cf890088f57ae6128ecf815a0474e39e1db
JARVIS <parth.verma96@gmail.com>
2018-06-19 13:38:17 +0530

    Data from a client is added for refernece of web app

---
+++ process.cs
@@ -0,0 +1,27 @@
+#TYPE Selected.System.Management.Automation.PSCustomObject
+"DisplayName","DisplayVersion","Publisher","InstallDate"
+,,,
+"Google Chrome","67.0.3396.87","Google Inc.","20171120"
+"Help & Manual 7","7.3.5","EC Software","20180524"
+"HelpNDoc 5.7.1.578 Personal Edition","5.7.1.578 Personal Edition","IBE Software","20180522"
+"HTML Help Workshop",,,
+"TeamViewer 13","13.0.6447","TeamViewer",
+,,,
+"Python 3.6.5 Tcl/Tk Support (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"HP Support Assistant","8.6.18.11","HP Inc.","20171120"
+"HP Support Solutions Framework","12.9.18.3","HP Inc.","20171120"
+"Python 3.6.5 Development Libraries (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Python 3.6.5 Core Interpreter (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Python 3.6.5 Utility Scripts (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Python 3.6.5 Documentation (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Google Update Helper","1.3.33.17","Google Inc.","20180522"
+"Python 3.6.5 Test Suite (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Python Launcher","3.6.6295.0","Python Software Foundation","20180618"
+"Python 3.6.5 Standard Library (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"CHM Viewer",,"chmviewer.com","20180524"
+"Python 3.6.5 Executables (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"HP Customer Experience Enhancements","6.0.11.1","HP Inc.","20171120"
+"Python 3.6.5 pip Bootstrap (32-bit)","3.6.5150.0","Python Software Foundation","20180618"
+"Intel(R) Processor Graphics","9.17.10.4229","Intel Corporation",
+"Google Talk Plugin","5.41.3.0","Google","20171120"
+"Intel(R) SDK for OpenCL - CPU Only Runtime Package","2.0.0.37149","Intel Corporation",
