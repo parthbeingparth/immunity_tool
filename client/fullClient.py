91562dadbec4a6d282d42efd081218712f1e0575
JARVIS <parth.verma96@gmail.com>
2018-06-19 13:36:58 +0530

    Client side updated

--- client/fullClient.py
+++ client/fullClient.py
@@ -1,3 +1,34 @@
-print ("\n\nTransfering data (software versions with local IP address) to Admin PC, Waiting for response \n\n---------------------------------------\n Handshake succesfull \n---------------------------------------")
-print("\n|########################################|100%% done")
-print("\nData sent succesfully\n\n")
+import uuid
+from uuid import getnode
+import socket
+import subprocess
+
+def get_mac():
+  mac_num = hex(uuid.getnode()).replace('0x', '').upper()
+  mac = ''.join(mac_num[i : i + 2] for i in range(0, 11, 2))
+  return mac
+
+a = get_mac()
+subprocess.call(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe","D:\client\pkgs\ClientF\process.ps1;"])
+s = socket.socket()
+
+#host = socket.gethostname()
+port = 60000
+
+s.connect(('192.168.173.220', port))
+s.send(str(a))
+
+
+with open('abcd.txt', 'rb') as f:
+	print("file opened")
+	l=f.read(1024)
+	while(1):
+		s.send(l)
+		print('sent: ', repr(l))
+		l=f.read(1024)
+		if not l:
+		 break
+	f.close()
+print('file sent')
+s.close()
+print('connection closed')
