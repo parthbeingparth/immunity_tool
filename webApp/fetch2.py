b4beec4ddd698ff509d61a29213e7cce183c172a
JARVIS <parth.verma96@gmail.com>
2018-06-21 02:23:19 +0530

    Adding status in table

--- webApp/fetch2.py
+++ webApp/fetch2.py
@@ -11,21 +11,23 @@ data = json.loads(temp.text)
 
 class ItemTable(Table):
     name = Col('Name')
-    version = Col('version')
+    version = Col('Version')
     date = Col('Date')
     vendor = Col('Vendor')
+    status = Col("Status")
 
 # Get some objects
 class Item(object):
-    def __init__(self, name, version, date, vendor):
+    def __init__(self, name, version, date, vendor, status):
         self.name = name
         self.version = version
         self.date = date
         self.vendor = vendor
+        self.status = status
 for i in range(len((data["data"][0]["list"]))):
     temp=list((data["data"][0]["list"][i]).values())
-    items.append(Item(temp[0],temp[1],temp[2],temp[3]))
+    items.append(Item(temp[0],temp[1],temp[2],temp[3],"Approved"))
 
 table = ItemTable(items)
 
-print(table)
+print(table.__html__())
